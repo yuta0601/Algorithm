0f741345797ea0f0213f24fecd5733e5db1b83f3
yuta0601 <y.nakazawa3003+github@gmail.com>
2020-10-08 00:00:42 +0900

    initial commit

---
+++ selection_sort.py
@@ -0,0 +1,20 @@
+
+# 選択ソートをコード化
+def sort(target):
+    for i in range(0, len(target)-1):
+        select_min(target, i)
+
+# targetのi番目以降の最初打を見つけi番目の値より小さい場合交換する
+def select_min(target, i):
+    min = i
+    for j in range(i+1, len(target)):
+        if target[min] > target[j]:
+            min = j
+        target[i], target[min] = target[min], target[i]
+
+if __name__=='__main__':
+    A = [8, 4, 3, 9, 6]
+
+    sort(A)
+
+    print(A)
