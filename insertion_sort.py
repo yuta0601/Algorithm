50bb404e13243987712fa378ab08858340e292e2
yuta0601 <y.nakazawa3003+github@gmail.com>
2020-10-08 23:18:12 +0900

    [ADD]挿入ソートを作成

---
+++ insertion_sort.py
@@ -0,0 +1,27 @@
+def sort(target):
+    for i in range(1, len(target)):
+        print(f'{target}変換対象：{target[i]}')
+        insert(target, i)
+
+def insert(target, i):
+    temp = target[i]
+    # スタート値: i-1, 終了値: -1, step: -1
+    # i-1から0まで逆順に見ていく
+    for j in range(i-1, -1, -1):
+        # tempより大きい値は右にひとつずらす
+        if temp < target[j]:
+            target[j+1] = target[j]
+        # tempより小さい値が見つかったらその次にtempを入れる
+        else:
+            target[j+1] = temp
+            # これ以上走査する必要がないのでbreak
+            break
+    # 繰り返し処理を最後まで終了した場合には一番最初にtempを入れる
+    else:
+        target[0] = temp
+
+if __name__=='__main__':
+    A = [6, 3, 8, 5, 7]
+    print(f'ソート前:{A}')
+    sort(A)
+    print(f'ソート後:{A}')
